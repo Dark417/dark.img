020150fb939099b6927a7dffd8aede5c93482095
Dark417 <dark417@163.com>
2017-04-18 23:47:04 +0800

    t

--- start.js
+++ start.js
@@ -1,3 +1,18 @@
 /**
  * Created by dark on 17/4/5.
  */
+console.log('oh yeah!');
+
+var http = require('http');
+var express = require('express');
+var path = require('path');
+var app = express();
+
+
+
+
+app.use('/', function(req,res) {
+  res.sendFile('2.htm', {root: path.join(__dirname, '/')});
+});
+
+app.listen(8888);
