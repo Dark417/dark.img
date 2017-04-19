4dbab3cfa4f81bc16d4207411cc86445f3e43377
Dark417 <dark417@163.com>
2017-04-19 12:16:14 +0800

    t

--- 2.js
+++ 2.js
@@ -15,4 +15,30 @@ var app = express();
 app.use(AV.express());
 
 
+app.use('/', function(req,res) {
+  res.send('Hello World');
+  // res.sendFile('index.html', {root: path.join(__dirname, '/')});
+  // res.writeHead(200, {'Content-Type': 'text/plain'});
+  console.log('use');
+
+});
+
+
+
+app.get('/', function(req,res) {
+  res.sendFile('index.html', {root: path.join(__dirname, '/')});
+  // res.sendFile('2.htm', { root: __dirname + '/' } );
+  // res.writeHead(200, {'Content-Type': 'text/plain'});
+  console.log('get');
+});
+
+
+
+
+
+
+
+
+
+
 app.listen(process.env.LEANCLOUD_APP_PORT);
