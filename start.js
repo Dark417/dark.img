5f50b24f3ebac5fa119cc1e00d3774cafeaf8ef2
Dark417 <dark417@163.com>
2017-04-19 11:57:13 +0800

    t

--- start.js
+++ start.js
@@ -19,13 +19,14 @@ var app = express();
 console.log(__dirname);
 
 app.use('/', function(req,res) {
-  res.sendFile('index.html', {root: path.join(__dirname, '/')});
+  res.send('Hello World');
+  // res.sendFile('index.html', {root: path.join(__dirname, '/')});
   // res.writeHead(200, {'Content-Type': 'text/plain'});
   console.log('use');
 
 });
 
-if('index.htm;'){
+if(index.html.exists()){
   console.log('2222');
 }
 //
